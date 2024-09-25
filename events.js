c1bf2d2d9d2c465fc33b1b6e8d87ec3e89176525
Kiyotoko <karl.zschiebsch@gmail.com>
2024-09-25 23:07:54 +0200

    Add code prettify

--- events.js
+++ events.js
@@ -2,3 +2,4 @@ function copy() {
     navigator.clipboard.writeText('xmlns:xsi="http://www.w3.org/2001/XMLSchema" xsi:schemaLocation="https://kiyotoko.github.io/eaw-xml-schema/schemas/%SCHEMA% https://kiyotoko.github.io/eaw-xml-schema/schemas/%SCHEMA%.xsd"')
     document.getElementById("copy").innerText = "Copied!"
 }
+
